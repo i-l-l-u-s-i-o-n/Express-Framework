98929db588d042d3af951cbef3fc877c20477cd6
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-09 18:12:29 +0000

    Conditionals and loops in EJS

---
+++ EJS_Conditionals_and_Loops/app.js
@@ -0,0 +1,27 @@
+var express=require("express"); 
+var app=express();
+
+app.get("/",function(req,res){
+    res.render("home.ejs");
+});
+
+app.get("/posts",function(req,res){
+    
+    var posts=[
+        {
+            title : "The amazing Spider man", author:"Colt"
+        },
+        {
+            title :"A quiet place", author:"Stan"
+        },
+        {
+            title :"The Funny cat", author:"Tim"
+        }];
+    
+    res.render("posts.ejs",{posts_details: posts});   // Pass parameter as the object.
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server Started");
+});
+
