b826b3c91595e85e92077efef989b4771087bc72
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-10 18:08:34 +0000

    Initial commit

--- Working with API's/SUNSET Time API/app.js
+++ Working with API's/SUNSET Time API/app.js
@@ -1,10 +1,8 @@
 var request=require("request");
 
-
-
 request("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2019-02-10",function(error,response,body){
      
-     //Checking if there is no error or status code is 200 i.e OK.
+     //Checking if there is no error and status code is 200 i.e OK.
     if (!error && response.statusCode == 200) {
         var parsedData= JSON.parse(body);
         console.log("SUNSET AND SUNRISE AT lat=36.7201600 & lng=-4.4203400 on date = 2019-02-10 is : ");
--- Working with API's/Using API in Express/app.js
+++ Working with API's/Using API in Express/app.js
@@ -10,7 +10,7 @@ var request=require("request");
 request("https://jsonplaceholder.typicode.com/users/1",function(error,response,body){
    
    
-   //Checking if there is no error or status code is 200 i.e OK.
+   //Checking if there is no error and status code is 200 i.e OK.
    if (!error && response.statusCode == 200) {
         var parsedData= JSON.parse(body);
         console.log(parsedData);
