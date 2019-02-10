c5f810f38829a8bfa69c2173e8f03063fac146ad
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-10 16:13:02 +0000

    Working with API

---
+++ Working with API's/Using API in Express/app.js
@@ -0,0 +1,15 @@
+// TO use any API , we have to amke request using "request" package 
+// It will return data in XML(Extended Markup Language) or JSON(JS object notation) as STRING.
+// We have to use JSON.parse() to parse the recieved data in an simple format(object).
+// We can also use "curl <url or API link>" to request from terminal.
+// To install request package- >
+// npm i request --save
+
+var request=require("request");
+
+request("https://jsonplaceholder.typicode.com/users/1",function(error,response,body){
+    var parsedData= JSON.parse(body);
+    console.log(parsedData);
+    console.log("Name : "+parsedData.name);
+    console.log("Zip code : "+ parsedData.address.zipcode);
+})
