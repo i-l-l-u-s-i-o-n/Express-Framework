2088161244b37a7c71edf4d73389be22bb3645bf
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-14 19:00:51 +0000

    Added some styling only

--- Working with API 's/MOVIE API App/app.js
+++ Working with API 's/MOVIE API App/app.js
@@ -5,10 +5,20 @@ var app=express();
 
 var request=require("request");
 
+
+app.use(express.static("public"));
+
 app.get("/",function(req,res){
-    res.send("Working");
+    res.render("home.ejs");
 });
+
+app.get("/result",function(req,res){
+    res.render("home.ejs")
+})
+
+
+
 
 app.listen(process.env.PORT,process.env.ID,function(){
     console.log("Server started");
-})
+});
