9af9172d0e4dcc870d6e3fd7da52717c079859c5
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-16 18:15:17 +0000

    Still some FrontEnd Left

--- Working with API 's/MOVIE API App/app.js
+++ Working with API 's/MOVIE API App/app.js
@@ -21,12 +21,10 @@ app.get("/result",function(req,res){
     
     
     var query=req.query.search;
-    console.log(typeof query);
     var url="http://www.omdbapi.com/?s="+query+"&apikey=thewdb"
     request(url,function(error,response,body){
         if (!error && response.statusCode == 200) {
             data=JSON.parse(body);
-            console.log(data);
             if(data.Response !=='False' ){
                 res.render("details.ejs",{data : data});
             }else{
@@ -37,8 +35,22 @@ app.get("/result",function(req,res){
 });
 
 
-app.get("/full_detail",function(req,res){
-    res.render("full_detail.ejs");
+app.get("/full_detail/:imdbId",function(req,res){
+    
+    var ID=req.params.imdbId;
+    
+    // Now requesting for the movie datails using IMDB id.
+    
+    var link="http://www.omdbapi.com/?i="+ID+"&apikey=thewdb";
+    console.log(ID);
+    
+    request(link,function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            data=JSON.parse(body);
+            console.log(data);
+            res.render("full_detail.ejs",{data : data});
+        }
+    });
 });
 
 app.listen(process.env.PORT,process.env.ID,function(){
---
+++ Working with API 's/MOVIE API App/full_detail.js
@@ -0,0 +1,3 @@
+var a=document.querySelector("a");
+a.setAttribute("href", "ss");
+console.log(a.getAttribute("href"));
